7105f3afa2223e7b772f444ee3097cf288cb7127
nasusa <lepolsussu@gmail.com>
2016-11-11 12:48:20 +0200

    Starting frontpage

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,13 +4,26 @@
 @section('description', 'This is a description')
 @section('keywords', 'These, are, keywords')
 @section('styles')
-    <!-- Custom Style or Css Here -->
+
 @stop
 @section('scripts_top')
-    <!-- Top Scripts Here -->
+
 @stop
 @section('content')
-    test<!-- Content Here -->
+
+<section class="mainhero">
+  <h1>Tampereen seudun ammattiopisto</h1>
+  <h2>LANPARTY</h2>
+</section>
+
+<section class="middleman">
+  
+  <a href="#" class="icon-button facebook"><i class="fa fa-facebook"></i><span></span></a>
+  <a href="#" class="icon-button instagram"><i class="fa fa-instagram"></i><span></span></a>
+  <a href="#" class="icon-button envelope-o"><i class="fa fa-envelope-o"></i><span></span></a> 
+  
+</section>
+
 @stop
 @section('scripts_bottom')
     <!-- Bottom Scripts Here -->
