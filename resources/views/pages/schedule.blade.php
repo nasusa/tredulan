bb2344139088fff496d4618e1cb42d190f8ffde0
Alan Aasmaa <alanaasmaa@gmail.com>
2016-12-01 20:50:26 +0200

    Poistettu isot iconit. Schedule sivulta.

--- resources/views/pages/schedule.blade.php
+++ resources/views/pages/schedule.blade.php
@@ -121,12 +121,6 @@
     </ul>   
   </li>       
 </ul> 
-  
- <div>
-  <a href="#" class="icon-button facebook"><i class="fa fa-facebook"></i><span></span></a>
-  <a href="#" class="icon-button instagram"><i class="fa fa-instagram"></i><span></span></a>
-  <a href="#" class="icon-button envelope-o"><i class="fa fa-envelope-o"></i><span></span></a> 
-  </div>
 </div>
 
 </section>
