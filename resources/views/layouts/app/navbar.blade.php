d74298c5d0a218f053f30e76fa1a89ce4992dbb5
nasusa <lepolsussu@gmail.com>
2017-01-11 11:06:03 +0200

    mobile

--- resources/views/layouts/app/navbar.blade.php
+++ resources/views/layouts/app/navbar.blade.php
@@ -1,17 +1,11 @@
  <nav>
     <section class="container">
         <div>
-            <span>
-            </span>
-        </div>
-        <div>
             <ul>
               <a href="/"> <li>ETUSIVU</li> </a>
               <a href="/guide"> <li>OPAS JA OHJELMA</li> </a>
               <a href="/aboutus"> <li>TIETOA MEISTÄ</li> </a>
             </ul>
-        </div>
-        <div>
         </div>
     </section>
 </nav>
