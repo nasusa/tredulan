aeda85ce3a9e7cb979332bc51b2f4c121ed608c7
nasusa <lepolsussu@gmail.com>
2017-01-12 21:07:54 +0200

    mobiilinavigointi

--- resources/views/layouts/app/navbar.blade.php
+++ resources/views/layouts/app/navbar.blade.php
@@ -3,7 +3,16 @@
         <div>
             <ul>
               <a href="/"> <li>ETUSIVU</li> </a>
-              <a href="/guide"> <li>OPAS JA OHJELMA</li> </a>
+                <li class ="mobilecontrol">OPAS JA OHJELMA <i class="fa fa-angle-right" aria-hidden="true"></i> </a>
+                    <ul>
+                        <li class="dropdown"><a href="/guide">Opas</a></li>
+                        <li class="dropdown"><a href="/info">Sijainti</a></li>
+                        <li class="dropdown"><a href="/rules">Säännöt</a></li>
+                        <li class="dropdown"><a href="/schedule">Aikataulu</a></li>
+                        <li class="dropdown"><a href="/tournaments">Kilpailut</a></li>
+                    </ul> 
+                </li>
+                <a href="/guide"> <li class="desktop">OPAS JA OHJELMA</a></li>
               <a href="/aboutus"> <li>TIETOA MEISTÄ</li> </a>
             </ul>
         </div>
