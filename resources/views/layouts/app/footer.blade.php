f9dbd74c4ee9f8407aaaeb84600278675beb9632
nasusa <lepolsussu@gmail.com>
2017-01-20 07:50:07 +0200

    lastone

--- resources/views/layouts/app/footer.blade.php
+++ resources/views/layouts/app/footer.blade.php
@@ -2,4 +2,5 @@
   <a target="_blank" href="https://www.facebook.com/groups/tredulanit2017/" class="icon-button facebook"><i class="fa fa-facebook"></i><span></span></a>
   <a target="_blank" href="https://www.instagram.com/tredulanit/" class="icon-button instagram"><i class="fa fa-instagram"></i><span></span></a>
   <a target="_blank" href="https://www.youtube.com/channel/UC0C-g13YPjm2P5QBVjlRtaQ" class="icon-button youtube-o"><i class="fa fa-youtube"></i><span></span></a>
+  <p class="copyright"><i class="fa fa-copyright" aria-hidden="true"></i>Susanna Lepola</p>
 </footer>
