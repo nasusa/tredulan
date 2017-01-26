f41330f747bb260aceb115ee7c0e92d11b7e324e
nasusa <lepolsussu@gmail.com>
2017-01-26 13:45:53 +0200

    paivitysfinal

--- resources/views/pages/schedule.blade.php
+++ resources/views/pages/schedule.blade.php
@@ -15,6 +15,7 @@
 
 <div class="schedulemiddle">
   <h1 class="scheduleh1"> AIKATAULU </h1>
+  <p class ="schedulep"> Aikatauluihin voi tulla muutoksia. </p>
   <ul class="main">
   <li class="date">
     <h3>Maaliskuu 24</h3>
@@ -25,45 +26,45 @@
       <li>
         <a href="#">
           <span class="event-time">20:00 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-name">Tapahtuma alkaa</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location">Sähkötalon ovet avataan</span>
         </a>
       </li>
        
        <li>
         <a href="#">
           <span class="event-time">21:00 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-name">TULOSSA</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location"><!--content--></span>
         </a>
       </li>
       
        <li>
         <a href="#">
           <span class="event-time">22:00 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-name">TULOSSA</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location"><!--content--></span>
         </a>
       </li>
       
        <li>
         <a href="#">
           <span class="event-time">22:30 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-name">TULOSSA</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location"><!--content--></span>
         </a>
       </li>
       
        <li>
         <a href="#">
           <span class="event-time">23:00 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-name">TULOSSA</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location"><!--content--></span>
         </a>
       </li>
     </ul>  
@@ -77,45 +78,45 @@
       <li>
         <a href="#">
           <span class="event-time">08:00 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-name">TULOSSA</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location"><!--content--></span>
         </a>
       </li>
        
        <li>
         <a href="#">
           <span class="event-time">10:00 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-name">-TULOSSA-</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location"><!--content--></span>
         </a>
       </li>
       
        <li>
         <a href="#">
           <span class="event-time">11:00 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-name">TULOSSA</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location"><!--content--></span>
         </a>
       </li>
       
        <li>
         <a href="#">
-          <span class="event-time">12:00 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-time">15:00 - </span>
+          <span class="event-name">TULOSSA</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location"><!--content--></span>
         </a>
       </li>
       
        <li>
         <a href="#">
-          <span class="event-time">14:00 - </span>
-          <span class="event-name">Kickoff Ceremony</span>
+          <span class="event-time">20:00 - </span>
+          <span class="event-name">Tapahtuma päättyy</span>
           <br />
-          <span class="event-location">Headquarters</span>
+          <span class="event-location">Tapahtuman purkaminen aloitetaan</span>
         </a>
       </li>
     </ul>   
